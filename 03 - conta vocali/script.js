ab6802687392b1668cd90c7ded4cb4c96d65c763
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-20 18:42:56 +0100

    [end] consegna 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function trovaVocali (parola){
+    
+    let vocali = 'aAeEiIoOuU'
+    let contavocali = 0;
+    
+    for(let i = 0; i < parola.length; i++){
+        if(vocali.indexOf(parola[i]) !== -1){
+            contavocali += 1;
+        }
+    }
 
+    return contavocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(trovaVocali(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -6,6 +6,7 @@ const name = 'Mario';
 // Dichiara la funzione qui.
 
 
+
 // Invoca la funzione qui e stampa il risultato in console
 
 
