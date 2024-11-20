7f05e4ec2762a169a8f5249ab7ba28ae69d138ea
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-20 17:15:36 +0100

    [end] consegna 2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function creaArrayNuovo(){
+    
+    const newArray = [];
+    
+    for(let i = 0; i < names.length; i++){
+        if(names[i].includes('A')){
+            newArray.push(names[i])
+        }
+    }
+
+    return newArray
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(creaArrayNuovo(names))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
